7a9d3c548b5622e569d26c9fd62eb55dce33a389
rnfltpgus <jpp901220@gmail.com>
2022-08-28 20:09:47 +0900

    🔥 Faet: 리스트 삭제 기능 추가

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,12 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <View style={styles.goalItem}>
-      <Text style={styles.goalText}>{props.text}</Text>
-    </View>
+    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+      <View style={styles.goalItem}>
+        <Text style={styles.goalText}>{props.text}</Text>
+      </View>
+    </Pressable>
   );
 }
 
@@ -18,6 +20,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#5e0acc",
   },
   goalText: {
-    color: "white",
+    color: "#fff",
   },
 });
